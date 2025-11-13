912791253b606aa2816f85d106c2c4430965da1d
Tomas Sabbavini <tomassabbavini@hotmail.com>
2025-11-13 19:19:24 -0300

    schema

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -4,15 +4,9 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const blogSchema = require('./models/blog').blogSchema
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = blogSchema
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
